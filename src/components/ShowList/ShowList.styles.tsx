21800cbbbe5ae43cb1460805f15118383789ece0
JP Marra <jpmarra@jps-macbook-pro.myfiosgateway.com>
2020-05-02 19:51:06 -0400

    remove unused import

--- src/components/ShowList/ShowList.styles.tsx
+++ src/components/ShowList/ShowList.styles.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components"
+import styled from "styled-components"
 import { motion } from "framer-motion"
 import { above, White, Black } from "../../styles/utilities"
 
