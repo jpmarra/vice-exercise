6c7ad97343b55f7709ca3ad88feff56c7439f005
JP Marra <jpmarra@jps-macbook-pro.myfiosgateway.com>
2020-05-02 15:48:01 -0400

    add config for tests

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
     testEnvironment: "jest-environment-jsdom",
     setupFiles: ["<rootDir>/setupTests.js"],
     setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+    preset: "ts-jest",
+    transform: {
+        "\\.(jpg|jpeg|png)$": "<rootDir>/fileTransformer.js",
+        "^.+\\.(js|jsx)?$": "babel-jest",
+    },
 }
